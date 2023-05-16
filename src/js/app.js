d6816f2168b900c2e06084c89a69f348411dd7b3
egorsv1985 <egorvs2009@gmail.com>
2023-05-16 15:05:19 +0300

    start

---
+++ src/js/app.js
@@ -0,0 +1,77 @@
+/*
+(i) Код попадает в итоговый файл,
+только когда вызвана функция,
+например flsFunctions.spollers();
+Или когда импортирован весь файл,
+например import "files/script.js";
+Неиспользуемый (не вызванный)
+код в итоговый файл не попадает.
+
+Если мы хотим добавить модуль
+следует его расскоментировать
+*/
+
+// Включить/выключить FLS (Full Logging System) (в работе)
+window["FLS"] = true;
+
+// Подключение основного файла стилей
+import "../scss/style.scss";
+
+import * as flsFunctions from "./files/functions.js";
+
+/* Проверка поддержки webp, добавление класса webp или no-webp для HTML */
+/* (i) необходимо для корректного отображения webp из css  */
+flsFunctions.isWebp();
+/* Добавление класса touch для HTML если браузер мобильный */
+// flsFunctions.addTouchClass();
+/* Добавление loaded для HTML после полной загрузки страницы */
+// flsFunctions.addLoadedClass();
+/* Модуль для работы с меню (Бургер) */
+flsFunctions.menuInit();
+
+
+
+// Плавная навигация по странице
+flsScroll.pageNavigation();
+
+// Функционал добавления классов к хедеру при прокрутке
+flsScroll.headerScroll();
+
+
+const body = document.body;
+const scrollUp = "scroll-up";
+const scrollDown = "scroll-down";
+let lastScroll = 0;
+
+window.addEventListener("scroll", () => {
+  const currentScroll = window.pageYOffset;
+  if (currentScroll <= 0) {
+    body.classList.remove(scrollUp);
+    return;
+  }
+
+  if (currentScroll > lastScroll && !body.classList.contains(scrollDown)) {
+    // down
+    body.classList.remove(scrollUp);
+    body.classList.add(scrollDown);
+    // lottiePlayer.play();
+  } else if (
+    currentScroll < lastScroll &&
+    body.classList.contains(scrollDown)
+  ) {
+    // up
+    body.classList.remove(scrollDown);
+    body.classList.add(scrollUp);
+    // lottiePlayer.stop();
+  }
+  lastScroll = currentScroll;
+});
+
+
+
+
+
+
+
+
+
